f0f9a5b789192c439c226a6cc1c8d37dc263d299
rafaelmoraes003 <rafaelsm003@gmail.com>
2025-01-11 01:13:05 -0300

    FEAT sql seeder file

---
+++ src/main/resources/import.sql
@@ -0,0 +1,57 @@
+INSERT INTO books  ( id , genre , title , publisher_id ) VALUES (1,"History","The Fall of the Roman Republic",NULL);
+INSERT INTO books  ( id , genre , title , publisher_id ) VALUES (2,"History","The Civil War",NULL);
+INSERT INTO books  ( id , genre , title , publisher_id ) VALUES (3,"History","The Rise and Fall of Alexandria: Birthplace of the Modern Mind",NULL);
+INSERT INTO books  ( id , genre , title , publisher_id ) VALUES (4,"Science Fiction","Dune",NULL);
+INSERT INTO books  ( id , genre , title , publisher_id ) VALUES (5,"Science Fiction","Neuromancer",NULL);
+INSERT INTO books  ( id , genre , title , publisher_id ) VALUES (6,"Science Fiction","The Left Hand of Darkness",NULL);
+INSERT INTO books  ( id , genre , title , publisher_id ) VALUES (7,"Fantasy","The Hobbit",NULL);
+INSERT INTO books  ( id , genre , title , publisher_id ) VALUES (8,"Fantasy","A Game of Thrones",NULL);
+INSERT INTO books  ( id , genre , title , publisher_id ) VALUES (9,"Fantasy","The Name of the Wind",NULL);
+INSERT INTO books  ( id , genre , title , publisher_id ) VALUES (10,"Biography","Steve Jobs",NULL);
+INSERT INTO books  ( id , genre , title , publisher_id ) VALUES (11,"Biography","The Diary of a Young Girl",NULL);
+INSERT INTO books  ( id , genre , title , publisher_id ) VALUES (12,"Biography","Becoming",NULL);
+INSERT INTO books  ( id , genre , title , publisher_id ) VALUES (13,"Mystery","Gone Girl",NULL);
+INSERT INTO books  ( id , genre , title , publisher_id ) VALUES (14,"Mystery","The Girl with the Dragon Tattoo",NULL);
+INSERT INTO books  ( id , genre , title , publisher_id ) VALUES (15,"Mystery","The Da Vinci Code",NULL);
+INSERT INTO books  ( id , genre , title , publisher_id ) VALUES (16,"Non-Fiction","Sapiens: A Brief History of Humankind",NULL);
+INSERT INTO books  ( id , genre , title , publisher_id ) VALUES (17,"Non-Fiction","Educated",NULL);
+INSERT INTO books  ( id , genre , title , publisher_id ) VALUES (18,"Non-Fiction","The Immortal Life of Henrietta Lacks",NULL);
+
+INSERT INTO `authors` (`id`, `name`, `nationality`) VALUES (1, "Plutarch", "Greek");
+INSERT INTO `authors` (`id`, `name`, `nationality`) VALUES (2, "Shelby Foote", "American");
+INSERT INTO `authors` (`id`, `name`, `nationality`) VALUES (3, "Justin Pollard", "British");
+INSERT INTO `authors` (`id`, `name`, `nationality`) VALUES (4, "Frank Herbert", "American");
+INSERT INTO `authors` (`id`, `name`, `nationality`) VALUES (5, "William Gibson", "Canadian-American");
+INSERT INTO `authors` (`id`, `name`, `nationality`) VALUES (6, "Ursula K. Le Guin", "American");
+INSERT INTO `authors` (`id`, `name`, `nationality`) VALUES (7, "J.R.R. Tolkien", "British");
+INSERT INTO `authors` (`id`, `name`, `nationality`) VALUES (8, "George R.R. Martin", "American");
+INSERT INTO `authors` (`id`, `name`, `nationality`) VALUES (9, "Patrick Rothfuss", "American");
+INSERT INTO `authors` (`id`, `name`, `nationality`) VALUES (10, "Walter Isaacson", "American");
+INSERT INTO `authors` (`id`, `name`, `nationality`) VALUES (11, "Anne Frank", "German-Dutch");
+INSERT INTO `authors` (`id`, `name`, `nationality`) VALUES (12, "Michelle Obama", "American");
+INSERT INTO `authors` (`id`, `name`, `nationality`) VALUES (13, "Gillian Flynn", "American");
+INSERT INTO `authors` (`id`, `name`, `nationality`) VALUES (14, "Stieg Larsson", "Swedish");
+INSERT INTO `authors` (`id`, `name`, `nationality`) VALUES (15, "Dan Brown", "American");
+INSERT INTO `authors` (`id`, `name`, `nationality`) VALUES (16, "Yuval Noah Harari", "Israeli");
+INSERT INTO `authors` (`id`, `name`, `nationality`) VALUES (17, "Tara Westover", "American");
+INSERT INTO `authors` (`id`, `name`, `nationality`) VALUES (18, "Rebecca Skloot", "American");
+
+INSERT INTO `publishers` (`id`, `name`, `address`) VALUES (1, "Penguin Classics", "80 Strand, London, UK");
+INSERT INTO `publishers` (`id`, `name`, `address`) VALUES (2, "Vintage Books", "1745 Broadway, New York, NY, USA");
+INSERT INTO `publishers` (`id`, `name`, `address`) VALUES (3, "HarperCollins", "195 Broadway, New York, NY, USA");
+INSERT INTO `publishers` (`id`, `name`, `address`) VALUES (4, "Chilton Books", "4330 Street Road, Philadelphia, PA, USA");
+INSERT INTO `publishers` (`id`, `name`, `address`) VALUES (5, "Gibson Books", "8th Ave, Vancouver, BC, Canada");
+INSERT INTO `publishers` (`id`, `name`, `address`) VALUES (6, "Ace Books", "1755 Broadway, New York, NY, USA");
+INSERT INTO `publishers` (`id`, `name`, `address`) VALUES (7, "Bantam Books", "1745 Broadway, New York, NY, USA");
+INSERT INTO `publishers` (`id`, `name`, `address`) VALUES (8, "Doubleday", "1745 Broadway, New York, NY, USA");
+INSERT INTO `publishers` (`id`, `name`, `address`) VALUES (9, "Simon & Schuster", "1230 Avenue of the Americas, New York, NY, USA");
+INSERT INTO `publishers` (`id`, `name`, `address`) VALUES (10, "Puffin Books", "80 Strand, London, UK");
+INSERT INTO `publishers` (`id`, `name`, `address`) VALUES (11, "Crown Publishing Group", "1745 Broadway, New York, NY, USA");
+INSERT INTO `publishers` (`id`, `name`, `address`) VALUES (12, "Penguin Random House", "1745 Broadway, New York, NY, USA");
+INSERT INTO `publishers` (`id`, `name`, `address`) VALUES (13, "Norstedts Förlag", "Luntmakargatan 38, Stockholm, Sweden");
+INSERT INTO `publishers` (`id`, `name`, `address`) VALUES (14, "Random House", "1745 Broadway, New York, NY, USA");
+INSERT INTO `publishers` (`id`, `name`, `address`) VALUES (15, "Broadway Books", "1745 Broadway, New York, NY, USA");
+
+
+
+
