ff9ad12c96cbe6f129c3993abd53aea52b5e7eab
rafaelmoraes003 <rafaelsm003@gmail.com>
2025-01-11 01:26:42 -0300

    FEAT book service :: findAll (overload with pagination)

--- src/main/java/com/alexandria/features/book/service/BookService.java
+++ src/main/java/com/alexandria/features/book/service/BookService.java
@@ -3,10 +3,12 @@ package com.alexandria.features.book.service;
 import com.alexandria.exception.ObjectNotFoundException;
 import com.alexandria.features.book.entity.Book;
 import com.alexandria.features.book.repository.BookRepository;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-;
 
 @Service
 public class BookService {
@@ -23,6 +25,17 @@ public class BookService {
   public Book findById(Long id) throws ObjectNotFoundException {
     return bookRepository.findById(id)
         .orElseThrow(() -> new ObjectNotFoundException("book not found."));
+  }
+
+  public List<Book> findAll() {
+    return bookRepository.findAll();
+  }
+
+  public List<Book> findAll(int pageNumber, int pageSize) {
+    Pageable pageable = PageRequest.of(pageNumber, pageSize);
+    Page<Book> page = bookRepository.findAll(pageable);
+
+    return page.toList();
   }
 
 }
