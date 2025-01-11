cd1fc03c710752e1a948749ca77e3c97d522e3dc
rafaelmoraes003 <rafaelsm003@gmail.com>
2025-01-11 01:33:50 -0300

    FEAT publisher repository

---
+++ src/main/java/com/alexandria/features/publisher/repository/PublisherRepository.java
@@ -0,0 +1,10 @@
+package com.alexandria.features.publisher.repository;
+
+import com.alexandria.features.publisher.entity.Publisher;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PublisherRepository extends JpaRepository<Publisher, Long> {
+
+}
